071187e6d634bf3108636e0b16d311a259624a56
Valerie <lera.stasyukevich@gmail.com>
2023-11-16 14:25:44 +0300

    Added task filtering by status

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -17,6 +17,13 @@ class Task(db.Model):
     status = db.relationship('TaskStatus', back_populates='tasks', lazy=True)
     user_tasks = db.relationship('UserTask', back_populates='task', cascade='all, delete-orphan')
 
+    def serialize(self):
+        return {
+            'task_id': self.task_id,
+            'description': self.description,
+            'deadline': self.deadline.strftime('%Y-%m-%d') if self.deadline else None,
+            'status': self.status.serialize() if self.status else None
+        }
 
 class TaskStatus(db.Model):
     __tablename__ = 'task_statuses'
@@ -24,6 +31,11 @@ class TaskStatus(db.Model):
     status_name = db.Column(db.String(50), nullable=False)
     tasks = db.relationship('Task', back_populates='status', cascade='all, delete-orphan')
 
+    def serialize(self):
+        return {
+            'status_id': self.status_id,
+            'status_name': self.status_name
+        }
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -33,7 +45,6 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     tasks = db.relationship('UserTask', back_populates='user', cascade='all, delete-orphan')
 
-
 class UserTask(db.Model):
     __tablename__ = 'user_tasks'
 
@@ -42,12 +53,11 @@ class UserTask(db.Model):
     user = db.relationship('User', back_populates='tasks')
     task = db.relationship('Task', back_populates='user_tasks')
 
-
 @app.route('/')
 def index():
     tasks = Task.query.all()
-    return render_template('index.html', tasks=tasks)
-
+    task_statuses = TaskStatus.query.all()
+    return render_template('index.html', tasks=tasks, task_statuses=task_statuses)
 
 @app.route('/add_task', methods=['GET', 'POST'])
 def add_task():
@@ -55,20 +65,13 @@ def add_task():
         description = request.form.get('description')
         deadline = request.form.get('deadline')
         status_id = request.form.get('status')
-
-        # Создание новой задачи
         new_task = Task(description=description, deadline=deadline, status_id=status_id)
-
-        # Добавление задачи в бд
         db.session.add(new_task)
         db.session.commit()
-
         return redirect(url_for('index'))
 
-    # Получение списка статусов задач из бд
     task_statuses = TaskStatus.query.all()
     return render_template('add_task.html', task_statuses=task_statuses)
-
 
 @app.route('/edit_task/<int:task_id>', methods=['GET', 'POST'])
 def edit_task(task_id):
@@ -79,27 +82,32 @@ def edit_task(task_id):
         task.description = request.form.get('description')
         task.deadline = request.form.get('deadline')
         task.status_id = request.form.get('status')
-
         db.session.commit()
-
         return redirect(url_for('index'))
 
     return render_template('edit_task.html', task=task, task_statuses=task_statuses)
-
 
 @app.route('/delete/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
     task = Task.query.get(task_id)
     if task:
-        # Удаление связанных записей в таблице user_tasks
         for user_task in task.user_tasks:
             db.session.delete(user_task)
-
         db.session.delete(task)
         db.session.commit()
         return jsonify({'message': 'Task deleted successfully'}), 200
     else:
         return jsonify({'message': 'Task not found'}), 404
+
+@app.route('/filter/<status_id>')
+def filter_tasks(status_id):
+    if status_id and status_id != 'all':
+        tasks = Task.query.filter_by(status_id=status_id).all()
+    else:
+        tasks = Task.query.all()
+
+    # Преобразуем результат в формат JSON и отправляем клиенту
+    return jsonify(tasks=[task.serialize() for task in tasks])
 
 
 if __name__ == '__main__':
